ba9ef373d7cdf17718e980ceb7d35a681bbcfbd1
BurakAlbas <100342354+BurakAlbas@users.noreply.github.com>
2022-02-24 11:56:31 +0300

    Add files via upload

---
+++ Sıra Al Sıra Çıkart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace queue
+{
+    public partial class Form1 : Form
+    {
+        Queue kuyruk = new Queue();
+        int sira = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSiraAl_Click(object sender, EventArgs e)
+        {
+            sira++;
+            kuyruk.Enqueue(sira);
+            Listele();
+        }
+        private void Listele()
+        {
+            listeSiralar.Items.Clear();
+            foreach (int sira in kuyruk)
+            {
+                listeSiralar.Items.Add(sira);
+            }
+        }
+
+        private void btnSiraCikart_Click(object sender, EventArgs e)
+        {
+            kuyruk.Dequeue();
+            Listele();
+        }
+    }
+}
